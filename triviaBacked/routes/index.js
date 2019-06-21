ef24d3d09861d2b39b1abd77b3d81def8e5d2fd5
noa-s <noa.fullstack@gmail.com>
2019-06-21 08:08:29 +0300

    remove unwanted remark

--- triviaBacked/routes/index.js
+++ triviaBacked/routes/index.js
@@ -5,8 +5,6 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-// router.get('/newQuiz', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+
 
 module.exports = router;
